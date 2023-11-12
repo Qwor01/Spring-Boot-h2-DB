2bcb9d5d468964f2c93a5814f586717829276dfd
Ignacio Esposito Busto <ieb@serviguide.net>
2023-11-12 14:55:23 +0100

    Base de datos h2

---
+++ src/test/java/CRUD/h2CRUD/H2CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package CRUD.h2CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
